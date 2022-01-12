959797e78df63b5e90d3ec2f5c1aa46032c173ff
irf98 <ignacioramosfarinho@gmail.com>
2022-01-12 02:33:09 -0300

    navbar

---
+++ src/components/nav-bar/navBar.js
@@ -0,0 +1,14 @@
+import './navBar.css';
+
+export default function NavBar() {
+  return (
+    <nav>
+        <ul>
+          <li> Home     </li>
+          <li> Products </li>
+          <li> Contact  </li>
+          <li> About us </li>
+        </ul>
+    </nav>
+  );
+}
