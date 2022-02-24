6dbb494379c4fc28946878f62d62cb83e4b12f1c
gonzalo <gonza0838@gmail.com>
2022-02-24 02:37:04 -0300

    styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,10 @@
 import './HomeStyles.css';
 
 const Home = () => {
-    return ( 
-    <div className="container"> 
-        <h1> Home page ! </h1> 
-    </div>
+    return (  
+        <div className="container"> 
+            <h1> Home page ! </h1> 
+        </div>
     )
 }
 
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -10,6 +10,10 @@ const Item = ({item}) => {
             <p> Descripcion: {item.description} </p>
             <p> Stock: <b>{item.stock}</b>      </p>
             <p> Precio: <b>{item.price}</b>    </p>
+            {/* item.pictureUrl en firebase esta definido como string y el valor es =
+            '../images/capsulaTres.jpg'  pero no aparece la imagen  */}
+            {/* <img src={require(`${item.pictureUrl}`)} alt='img'/> */}
+            <img src={require('../images/capsulaTres.jpg')} alt='capsula'/>
             <button onClick={()=> navigate(`/products/${item.id}`)}> Ver detalles </button>
         </div>
     );
